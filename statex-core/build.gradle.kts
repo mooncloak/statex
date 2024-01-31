8ad98eb078db60b90e39868beafbf7ab7b28c528
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2024-01-30 20:13:58 -0500

    Added missing Android lifecycle viewmodel dependency

--- statex-core/build.gradle.kts
+++ statex-core/build.gradle.kts
@@ -69,6 +69,12 @@ kotlin {
             }
         }
 
+        val androidMain by getting {
+            dependencies {
+                implementation(AndroidX.lifecycle.viewModelKtx)
+            }
+        }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
