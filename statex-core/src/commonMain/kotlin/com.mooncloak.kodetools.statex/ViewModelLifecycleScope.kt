3ca562579aa37b79e381656b4326579aedca9e16
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2024-03-04 09:15:01 -0500

    Updated documentation of ViewModelLifecycleScope

--- statex-core/src/commonMain/kotlin/com.mooncloak.kodetools.statex/ViewModelLifecycleScope.kt
+++ statex-core/src/commonMain/kotlin/com.mooncloak.kodetools.statex/ViewModelLifecycleScope.kt
@@ -2,11 +2,30 @@ package com.mooncloak.kodetools.statex
 
 import androidx.compose.runtime.State
 
+/**
+ * Defines the lifecycle for a [ViewModel].
+ */
 interface ViewModelLifecycleScope {
 
+    /**
+     * A [State] determining whether the [ViewModel] component containing this lifecycle, is
+     * currently bound to the lifecycle of a containing component, such as a Composable function.
+     * If the [State.value] is `true`, then it is safe to invoke operations on the [ViewModel],
+     * otherwise, no operations on the [ViewModel] should be invoked because they will not emit the
+     * state values correctly to any subscribers.
+     */
     val isBound: State<Boolean>
 
+    /**
+     * Bind this [ViewModel] to the scope of the calling containing component. If the [ViewModel]
+     * with this [ViewModelLifecycleScope] is already bound, then this function will do nothing.
+     */
     fun bind()
 
+    /**
+     * Unbind this [ViewModel] from the scope of the calling containing component. If the
+     * [ViewModel] with this [ViewModelLifecycleScope] is already not bound, then this function
+     * will do nothing.
+     */
     fun unbind()
 }
